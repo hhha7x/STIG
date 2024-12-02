31a89cec190e5f0eac07f1639c7451b782aacce1
net_engineer_guy <50239141+hhha7x@users.noreply.github.com>
2024-12-02 08:27:11 -0500

    Add files via upload

---
+++ IOSXE_NDM_SWITCH_variables.py
@@ -0,0 +1,689 @@
+FileIOSXESWITCHNDM = input("What is the name of the IOSXE NDM ckl file you are editing?")
+# HostIP = input("What is the IP address of the device you want to STIG?")
+# Hostname = input("What is the hostname of the devices you want to STIG")
+
+# Target_Data = {
+#     "host_name": Hostname,
+#     "host_ip":  HostIP
+# }
+
+
+
+V_220518_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220518",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220518_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220518",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220519_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220519",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220519_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220519",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220520_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220520",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220520_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220520",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220521_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220521",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220521_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220521",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220522_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220522",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220522_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220522",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220523_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220523",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220523_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220523",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220524_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220524",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220524_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220524",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220525_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220525",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220525_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220525",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220526_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220526",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220526_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220526",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220528_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220528",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220528_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220528",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220529_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220529",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220529_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220529",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220530_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220530",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220530_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220530",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220531_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220531",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220531_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220531",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220531_NA = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220531",
+    "status": "Not_Applicable",
+    "finding_details": " ",
+    "comments": "Logging persistent is not being used "
+}
+
+V_220532_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220532",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220532_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220532",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220532_NA = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220532",
+    "status": "Not_Applicable",
+    "finding_details": " ",
+    "comments": "Logging persistent is not being used "
+}
+
+V_220533_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220533",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220533_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220533",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220533_NA = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220533",
+    "status": "Not_Applicable",
+    "finding_details": " ",
+    "comments": " Logging persistent is not being used"
+}
+
+V_220534_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220534",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220534_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220534",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220535_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220535",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+V_220535_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220535",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220537_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220537",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+V_220537_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220537",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220538_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220538",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+V_220538_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220538",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220539_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220539",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+V_220539_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220539",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220540_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220540",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+V_220540_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220540",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220541_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220541",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+V_220541_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220541",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220542_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220542",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+V_220542_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220542",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220543_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220543",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+V_220543_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220543",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220544_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220544",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+V_220544_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220544",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220545_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220545",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+V_220545_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220545",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220547_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220547",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+V_220547_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220547",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220548_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220548",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+V_220548_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220548",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220549_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220549",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+V_220549_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220549",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220552_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220552",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+V_220552_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220552",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220553_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220553",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+V_220553_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220553",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220554_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220554",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+V_220554_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220554",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220555_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220555",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+V_220555_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220555",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220556_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220556",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+V_220556_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220556",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220559_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220559",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+V_220559_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220559",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220560_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220560",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+V_220560_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220560",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220561_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220561",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+V_220561_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220561",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220565_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220565",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+V_220565_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220565",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220566_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220566",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+V_220566_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220566",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": "Backups are being done with Cisco Prime "
+}
+
+V_220567_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220567",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+V_220567_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220567",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220567_NA = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220567",
+    "status": "Not_Applicable",
+    "finding_details": " ",
+    "comments": "Certificates not being used. "
+}
+
+V_220568_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220568",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+V_220568_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220568",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
+V_220569_Open = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220569",
+    "status": "Open",
+    "finding_details": " ",
+    "comments": " "
+}
+V_220569_NotAFinding = {
+    "file_name": FileIOSXESWITCHNDM,
+    "key": "V-220569",
+    "status": "NotAFinding",
+    "finding_details": " ",
+    "comments": " "
+}
+
